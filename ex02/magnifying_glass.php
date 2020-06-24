1fce474cfe56da9c3d41c8a150c300ca03282ef3
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-06-25 02:26:24 +0300

    It's 2:25 AM and this isn't as fun anymore.

--- ex02/magnifying_glass.php
+++ ex02/magnifying_glass.php
@@ -23,15 +23,54 @@
 		echo "No tags found!\n";
 	*/
 
+	function upper(array $match): string
+	{
+		return strtoupper($match[0]);
+	}
+
+	function nupper(string &$string, int $offset, int $n)
+	{
+		for ($i = 0; $i < $n; ++$i)
+		{
+			$char = $string[$offset + $i];
+			if (ctype_lower($char))
+				$string[$offset + $i] = strtoupper($char);
+		}
+	}
+
 	$file = file_get_contents($argv[1]);
 
-	$file = preg_replace_callback(
-		"/<a.*?>\K.*?<|title=\K\".*\"/",
-		function ($m)
-		{
-			return strtoupper($m[0]);
-		},
-		$file);
+	$rgx_title = '/title="\K.*?(?=")/s';
+	$rgx_a_tags = '/<a .*?>(.*?)<\/a>/s';
+	$rgx_a_inner = '/(.+?)(?=<)(.+?>)|.+?(?(?=<)|.+)/s';
 
+	$file = preg_replace_callback($rgx_title, "upper", $file);
+
+	preg_match_all($rgx_a_tags, $file, $tag_content, PREG_OFFSET_CAPTURE);
+
+	foreach ($tag_content[1] as $content)
+	{
+		$base = $content[1];
+		preg_match_all($rgx_a_inner, $content[0], $pieces, PREG_OFFSET_CAPTURE);
+
+		$arrstr = $pieces[0][0][0];
+		if (strpos($arrstr, "<") == FALSE)
+		{
+			$offset = $base + $pieces[0][0][1];
+			nupper($file, $offset, strlen($arrstr));
+		}
+		else
+		{
+			foreach($pieces[1] as $fragment)
+			{
+				if (!empty($fragment))
+				{
+					$arrstr = $fragment[0];
+					$offset = $base + $fragment[1];
+					nupper($file, $offset, strlen($arrstr));
+				}
+			}
+		}
+	}
 	echo $file;
 ?>
