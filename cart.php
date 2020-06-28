2bb3015b530f1b2f9798daab537ef630c4eae46e
Yan Yan <yyan@c1r5p20.hive.fi>
2020-06-28 11:34:14 +0300

    cart implemented

--- cart.php
+++ cart.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	$session = $_SESSION;
+?>
+
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="style.css">
@@ -13,32 +18,54 @@
 		<div id="categories" class="big"></div>
 		<div id="site-wrapper">
 			<!-- content begin -->
-			<table id="cart-content">
+			
+			<table id=cart-content> 
 				<tr class="cart-item">
-					<td class="name">Product name</td>
-					<td class="total"></td>
-					<td class="price">price</td>
-					<td class="count">count</td>
+					<td class="name">Name </td>
+					
+					<td class="price">Item price</td>
+					<td class="count">Number of item</td>
+					<td class="total">Total price for each item</td>
 				</tr>
+			<?php
+
+
+				foreach ($session['cart'] as $item)
+				{
+					$link = mysqli_connect("localhost:3306", "root", 'password', "rush00");
+					if (!$link){
+						die('Could not connect: ' . mysqli_connect_error());
+					}
+					if (is_array($session[cart])) {
+						foreach ($session[cart] as $id => $num){
+							$query = "SELECT * FROM product WHERE id=$id";
+							$data = mysqli_query($link, $query);
+							if ($data){
+								$item =  mysqli_fetch_array($data, MYSQLI_ASSOC);
+							}
+							echo '<tr class="cart-item">';
+							echo '<td class="name">'. $item['name']. '</td>';
+							
+							echo '<td class="price">'.$item['price'].'</td>';
+
+							echo '<td class="count">'.$num.'</td>';
+							echo '<td class="total">'.$item['price']*$num.' €</td>';
+							echo '</tr>';
+						}
+					}
+				}
+			?> 
 				<tr class="cart-item">
-					<td class="name">Product name</td>
-					<td class="total"></td>
-					<td class="price">price</td>
-					<td class="count">count</td>
-				</tr>
-				<tr class="cart-item">
-					<td class="name">Product name</td>
-					<td class="total"></td>
-					<td class="price">price</td>
-					<td class="count">count</td>
-				</tr>
-				<tr class="cart-item">
+					<td class="name">Total</td>
 					<td></td>
 					<td></td>
-					<td></td>
-					<td class="total">total</td>
+					<td class="total">
+					<?php 
+					echo $session['total_price']." €";
+					?></td>
 				</tr>
 			</table>
+				
 			<a id="checkout" href="checkout.php">Checkout</a>
 			<!-- content end -->
 		</div>
